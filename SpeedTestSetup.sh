a63671c44a38abe43e3b5b5bfc78723ded2eabea
Chad Oftedahl <9145108+Firenza@users.noreply.github.com>
2020-07-12 11:35:52 -0700

    Create SpeedTestSetup.sh

---
+++ SpeedTestSetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/pedrocesar-ti/internet-speedtest-docker.git
+cd internet-speedtest-docker
+
+# Need step here to update the docker-compose.yml file to use locally built images then everything should be good
+
+docker-compose up -d
